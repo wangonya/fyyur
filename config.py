ae1f61dd4301801cd274e3609da2bec6896628d6
wangonya <kwangonya@gmail.com>
2019-10-27 10:47:37 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+SECRET_KEY = os.urandom(32)
+# Grabs the folder where the script runs.
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Enable debug mode.
+DEBUG = True
+
+# Connect to the database
+SQLALCHEMY_DATABASE_URI = os.getenv('DB_URI')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
