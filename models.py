0c9f96814b33197dc4bff702920df548844b7377
wangonya <kwangonya@gmail.com>
2019-11-02 14:39:11 +0300

    enable saving list to db

--- models.py
+++ models.py
@@ -15,7 +15,7 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-    genres = db.Column(db.String(120), nullable=False)
+    genres = db.Column(db.ARRAY(db.String), nullable=False)
     website = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean, default=False)
     seeking_description = db.Column(db.String)
