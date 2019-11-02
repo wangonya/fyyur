4d5defd83f0d7bf63633e2aeb333a7d5ff6cacb2
wangonya <kwangonya@gmail.com>
2019-11-02 14:37:24 +0300

    add getSelected function
    returns array of selected values in multi-select options

--- static/js/script.js
+++ static/js/script.js
@@ -3,4 +3,11 @@ window.parseISOString = function parseISOString(s) {
   return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
 };
 
-console.log('hey from script')
+getSelected = value => {
+  const selected = []
+  for (let i = 0; i < value.length; i++) {
+    if (value.options[i].selected) selected.push(value.options[i].value);
+  }
+  console.log('selected ==>', selected)
+  return selected
+}
