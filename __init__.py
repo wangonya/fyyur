82b984b39f8b6875c17cb51b35a57601d44d21e3
wangonya <kwangonya@gmail.com>
2019-11-03 06:30:13 +0300

    add cors

--- __init__.py
+++ __init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_moment import Moment
+from flask_cors import CORS
 
 from .config import Config
 
 app = Flask(__name__)
+CORS(app)
 moment = Moment(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
